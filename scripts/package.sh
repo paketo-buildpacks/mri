117c93d5316a69276756882bf3d7d606a0a7dc90
Danny Joyce <djoyce@pivotal.io>
2019-02-08 15:26:28 -0500

    Add common scripts
    Co-authored-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- scripts/package.sh
+++ scripts/package.sh
@@ -15,8 +15,11 @@ echo -n "Copying buildpack.toml..."
 cp buildpack.toml $bp_dir/buildpack.toml
 echo "done"
 
-# TODO: Update list of built binaries as they are written
-for b in detect build; do
+if [ "${BP_REWRITE_HOST:-}" != "" ]; then
+    sed -i -e "s|^uri = \"https:\/\/buildpacks\.cloudfoundry\.org\(.*\)\"$|uri = \"http://$BP_REWRITE_HOST\1\"|g" "$bp_dir/buildpack.toml"
+fi
+
+for b in $(ls cmd); do
     echo -n "Building $b..."
     GOOS=$TARGET_OS go build -o $bp_dir/bin/$b ./cmd/$b
     echo "done"
