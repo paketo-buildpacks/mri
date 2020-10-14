56cc9df6803f2fd53c98b5e6d30317623e3d06f5
Ryan Moran <rmoran@vmware.com>
2020-10-14 11:05:18 -0700

    Uses WithPullPolicy("never") instead of WithNoPull()

--- integration/simple_app_test.go
+++ integration/simple_app_test.go
@@ -58,7 +58,7 @@ func testSimpleApp(t *testing.T, context spec.G, it spec.S) {
 
 			var logs fmt.Stringer
 			image, logs, err = pack.WithNoColor().Build.
-				WithNoPull().
+				WithPullPolicy("never").
 				WithBuildpacks(
 					settings.Buildpacks.MRI.Online,
 					settings.Buildpacks.BuildPlan.Online,
